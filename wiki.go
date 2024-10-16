92a2a61cf81e32a7615c150f850943aaddc9a9ee
Klee Collage <klee.collage@gmail.com>
2024-10-15 20:30:30 -0500

    Application Web

---
+++ wiki.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"text/template"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: title, Body: body}, nil
+}
+
+// PATH VALIDATION
+var validPath = regexp.MustCompile("^/(edit|view|save)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("titulo de pagina invalido")
+	}
+	return m[2], nil
+}
+
+// FUNC CLOSURE
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+// CONTROLLER
+func handler(w http.ResponseWriter, r *http.Request) {
+	// en el request capturamos el path si incluir la posicion 1
+	fmt.Fprintf(w, "¡Hola! Me encantan los %s", r.URL.Path[1:])
+}
+
+// CONTROLLER
+func viewHandler0(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
+}
+
+// CONTROLLER
+func editHandler0(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+// CONTROLLER
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+// CONTROLLER
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	renderTemplate(w, "edit", p)
+}
+
+// CONTROLLER
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate0(w http.ResponseWriter, tmpl string, p *Page) {
+	t, err := template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// CACHE DE PLANTILLAS
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	// p1 := &Page{Title: "TestPage", Body: []byte("Esta es una pagina de muestra")}
+	// p1.save()
+
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
+
+	// Respuesta al cliente
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	// Creacion del servidor
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
